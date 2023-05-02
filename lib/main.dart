9f85f6c75c1229bc09c6203afee8fb0627dd6f55
Pedro Silva <arbeit.pedro.silva@gmail.com>
2023-05-01 21:13:31 -0300

    try commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Exemple',
+      value: '310.76',
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Test',
+      value: '211.30',
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bills profile'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.red,
+              child: Text('Grafic'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Text(tr.title),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/models/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String id;
+  final String title;
+  final String value;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.value,
+    required this.date,
+  });
+}
